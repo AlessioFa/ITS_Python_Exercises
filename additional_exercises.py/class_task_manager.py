32797944052fac7a5ad0bb9c8dec199ea2550dcc
AlessioFa <ale.farallo@gmail.com>
2025-09-30 15:34:21 +0200

    Small changes to a class exercise.

--- additional_exercises.py/class_task_manager.py
+++ additional_exercises.py/class_task_manager.py
@@ -5,24 +5,24 @@ task: dict[str, dict]
 """
 
 
-class TaskMAnager:
+class TaskManager:
 
     # nell' init passo tasks perchè posso voler passare il dizionario in input, sennò posso mettere solo il self e a quel punto devo inizializzare self.task = {}
     def __init__(self, tasks: dict[str, dict[str, str | bool]]):
 
         self.tasks: dict[str, dict[str, str | bool]] = {}
-    
+
     def create_task(self, task_id: str, description: str) -> dict[str, dict[str, str | bool]]:
 
         if task_id in self.tasks:
-            
+ 
             self.tasks[task_id]["Completato"] = True
 
-            # per costruzione è meglio aggiungere task_id: poichè l esercizio vuole un nuovo dizionario
+            # per costruzione si deve aggiungere task_id: poichè l esercizio vuole un nuovo dizionario
             return {task_id: self.tasks[task_id]}
 
         else:
-            # se l esercizio avesse voluto un eccezione : raise KeyError("Chiave non presente!")
+            # se l esercizio avesse chiesto un eccezione: raise KeyError("Chiave non presente!")
             return "Task not found!"
 
     def update_description(self, task_id: str, new_description: str) -> dict[str, dict[str, str | bool]]:
@@ -32,7 +32,6 @@ class TaskMAnager:
             return "Task not found."
 
         else:
-
             self.tasks[task_id]["Description"] = new_description
 
             return {task_id: self.tasks[task_id]}
@@ -44,7 +43,10 @@ class TaskMAnager:
             return "Task not found!"
 
         else:
-            # si potrebbe usare .popitem che restituisce una tupla , la aprte a sinistra della tupla èp la chiave associata al valore nel dizionario , la aprte di destra è il valore associato a quella chiave
+            """
+            si potrebbe usare .popitem che restituisce una tupla , la parte a sinistra della tupla
+            sarebbe la chiave associata al valore nel dizionario mentre la parte di destra il valore associato alla chiave stessa
+            """
             value = self.tasks.pop(task_id)
 
             # ritorno un dizionario che ritorna solo quella chiave e valore 
@@ -63,5 +65,4 @@ class TaskMAnager:
             return "Error, task not found."
 
         else:
-
             return {task_id: self.tasks[task_id]}
